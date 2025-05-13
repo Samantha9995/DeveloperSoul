661b22f9abdb5890395085ba584bf9660e695f9d
SADev <47018729+Samantha9995@users.noreply.github.com>
2025-05-13 18:26:59 +0800

    Add about me

---
+++ lib/about_me.dart
@@ -0,0 +1,96 @@
+// import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
+// import 'package:url_launcher/url_launcher.dart';
+
+// Copyright (c) 2025 SADev. All rights reserved.
+
+class AboutMePage extends StatelessWidget {
+  const AboutMePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(children: [
+        Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 17, 56, 186),
+              Color.fromARGB(255, 109, 190, 237)
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+          ),
+          child: Center(
+            child: Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                        'https://avatars.githubusercontent.com/u/47018729?s=400&u=ac0b960ea6d173bf2403ab5e9a465071665e6da4&v=4'),
+                  ),
+                  const SizedBox(height: 16),
+                  const Text('SADev',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                  const Text('Android & Flutter Developer',
+                      style: TextStyle(fontSize: 16, color: Colors.white)),
+                  const SizedBox(height: 16),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 32.0),
+                    child: Text(
+                      "Hey! I'm Samantha, an Android & Flutter Developer working on this Quiz app. Feel free to reach out! Check out my GitHub and I hope you enjoy using this app!",
+                      textAlign: TextAlign.center,
+                      style: TextStyle(fontSize: 14, color: Colors.white),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                          icon: Image.asset(
+                            'assets/images/github_icon.png',
+                            width: 24,
+                            height: 24,
+                            color: Colors.white,
+                          ),
+                          onPressed: () async {
+                            // Mark the function as async
+                            final Uri url =
+                                Uri.parse('https://github.com/Samantha9995');
+                            // if (await canLaunchUrl(url)) {
+                            //   // Check if the URL can be launched
+                            //   await launchUrl(url); // Launch the URL
+                            // } else {
+                            //   // Handle the error if the URL couldn't be launched
+                            //   logger.e('Could not launch gitURL');
+                            //   // Optionally, show an error message to the user using a SnackBar or AlertDialog
+                            // }
+                          }),
+                    ],
+                  ),
+                  const SizedBox(height: 32),
+                  const Text('Copyright © 2025 SADev',
+                      style: TextStyle(fontSize: 12, color: Colors.white)),
+                ],
+              ),
+            ),
+          ),
+        ),
+        Positioned(
+          top: 16,
+          left: 16,
+          child: IconButton(
+            icon: const Icon(Icons.close, color: Colors.white),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+        ),
+      ]),
+    );
+  }
+}
--- lib/main_quiz.dart
+++ lib/main_quiz.dart
@@ -1,4 +1,4 @@
-
+import 'package:adv_basics/about_me.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -9,49 +9,75 @@ class MainQuiz extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset(
-                  'assets/images/quiz-logo.png',
-                  width: 250,
-                  color: const Color.fromARGB(198, 255, 255, 255)
-                ),
-                // const Padding(padding: EdgeInsets.all(32.0)),
-                const SizedBox(height: 20),
-                Text(
-                  'What kind of programmer soul are you?',
-                  textAlign: TextAlign.center,
-                   style: GoogleFonts.lato(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                  )
-                ),
-                const SizedBox(height: 30),
-                OutlinedButton.icon(
-                  onPressed: startQuiz,
-                  style: OutlinedButton.styleFrom(
-                    foregroundColor: Colors.white,
-                    backgroundColor: const Color.fromARGB(55, 98, 134, 241), 
-                    textStyle: const TextStyle(
-                       fontSize: 16,
-                       fontWeight: FontWeight.bold
-                      ),
-                    side: const BorderSide(
-                      color: Color.fromARGB(255, 255, 255, 255),
-                      width: 1
-                    ),
-                  ),
-                  icon: const Icon(Icons.arrow_right_alt),
-                  label: const Text('Start Quiz'),
-                )
-              ],
-    )
-    );
+    return 
+    Container(
+      width: double.infinity,
+      height: double.infinity,
+
+    child: Stack(children: [
+      Center(
+        child:
+      Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        // mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset('assets/images/quiz-logo.png',
+              width: 250, color: const Color.fromARGB(198, 255, 255, 255)),
+          // const Padding(padding: EdgeInsets.all(32.0)),
+          const SizedBox(height: 20),
+          Padding(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            child: Text('What kind of programmer soul are you?',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.lato(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                )),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              backgroundColor: const Color.fromARGB(55, 98, 134, 241),
+              textStyle:
+                  const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+              side: const BorderSide(
+                  color: Color.fromARGB(255, 255, 255, 255), width: 1),
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      )
+      ),
+      Positioned(
+        top: 16,
+        right: 16,
+        child: PopupMenuButton<String>(
+          icon: const Icon(Icons.more_vert, color: Colors.white),
+          onSelected: (String item) {
+            switch (item) {
+              case 'About':
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const AboutMePage()));
+                break;
+            }
+          },
+          itemBuilder: (BuildContext context) {
+            return [
+              const PopupMenuItem<String>(
+                value: 'About',
+                child: Text('About the developer'),
+              ),
+            ];
+          },
+        ),
+      )
+    ])
+        );
   }
-  
 }
